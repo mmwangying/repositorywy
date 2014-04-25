3233759c3db32b0b82a78d1a1780939e132522f9
mmwangying <wangying@maimiaotech.com>
2014-04-25 11:24:15 +0800

    list operation

---
+++ list.py
@@ -0,0 +1,11 @@
+#Filename: list.py
+def deleteDuplicate(list):
+    if len(list) == 0:
+        return list
+    new_list = []
+    for value in list:
+        if new_list.count(value) == 0:
+            new_list.append(value)
+    return new_list
+
+print deleteDuplicate([1,1,2,1,3,4,2,4])
