ad44bdfef0fe96b5f5001433ff457a9201bd1661
mmwangying <wangying@maimiaotech.com>
2014-04-28 09:56:42 +0800

    rename ipcheck_shell

--- ipcheck_shell.sh
+++ ipcheck_shell.sh
@@ -1,11 +1,11 @@
-## #5.有如下log文件，请打印出独立IP，并统计独立IP数，（分别使用python和shell），检测异常ip（1分钟内访问超过30次） ##
-
+#!/bin/bash
 awk -F " " '{print $1" "$4}' log | awk -F "[" '{print $1" "$2}' | awk -F ":" '{print $1":"$2":"$3}' > newlog
-#打印独立的ip
-cat newlog | sort -t " " -k1,1 -u
-#统计独立ip的个数
-cat newlog | sort -t " " -k1,1 -u | wc -l
-#统计1分钟内，
-cat newlog | awk '{print $1$2}' | awk '{a[$1]++}END{for(i in a){print i " " a[i]}}'
 
+echo "The unique ip address is: "
+cat newlog | sort -t " " -k1,1 -u
+echo -e "\n" "The number of unique ip address is: "
+cat newlog | sort -t " " -k1,1 -u | wc -l
+echo -e "\n" "The visit time of ip address in one minute is: "
+cat newlog | awk '{print $1$2}' | awk '{a[$1]++}END{for(i in a){print i " " a[i]}}'
+echo -e "\n" "The abnormal ip address is: "
 cat newlog | awk '{print $1$2}' | awk '{a[$1]++}END{for(i in a){print i " " a[i]}}' | grep -n ' [3-9][0-9]\{1\}'
